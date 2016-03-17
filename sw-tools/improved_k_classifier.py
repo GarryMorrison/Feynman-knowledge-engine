b8ffad1073468c1cc552b32a43b493724f3bbb71
Garry Morrison <garry@semantic-db.org>
2016-03-18 01:21:09 +1030

    tidied up improved_k_classifier.py

--- sw-tools/improved_k_classifier.py
+++ sw-tools/improved_k_classifier.py
@@ -6,10 +6,21 @@
 # Full Feynman Knowledge Engine is here:
 # https://github.com/GarryMorrison/Feynman-knowledge-engine
 #
+# Hrmm... I wonder if standard dictionaries are faster than OrderedDict's? Since unlike the full project, we don't need superposition ordering information.
+# Yup. Cut run time in half for Ramsey-graph-1.sw and Ramsey-graph-2.sw for k = 44
+# from "real 0m3.743s" to "real 0m1.553s"
+# BTW, full project version:
+# $ time ./fast_k_classifier.py 44 sw-examples/Ramsey-graph-*.sw
+# took "real 0m12.010s"
+#
+# I now have two versions of node_to_signature().
+# I'm not sure which is faster, or if they are roughly the same.
+# and with v_list.reverse() switched off, they give the same signature.
+#
 # Author: Garry Morrison
 # email: garry -at- semantic-db.org
 # Date: 2016-03-17
-# Update:
+# Update: 2016-03-18
 # Copyright: GPLv3
 #
 # Usage: ./improved_k_classifier.py k network-1.sw [network-2.sw network-3.sw ...]
@@ -37,6 +48,7 @@ primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
 primes += [127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229]
 primes += [233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349]
 primes += [353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463]
+primes += [467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601]
 
 
 # check we have enough primes:
@@ -52,7 +64,8 @@ m = 4257452468389
 
 class superposition(object):
   def __init__(self,in_list=[]):
-    self.odict = OrderedDict()
+#    self.odict = OrderedDict()
+    self.odict = {}
     for x in in_list:
       if x != '':
         if x in self.odict:
@@ -100,7 +113,8 @@ class superposition(object):
 
 class new_context(object):
   def __init__(self):
-    self.rules = OrderedDict()
+#    self.rules = OrderedDict()
+    self.rules = {}
 
   # we want to learn simple rules like these:
   # op |1> => |2> + |3> + |5> + |9> + |10> + |14> + |16> + |17>
@@ -110,7 +124,7 @@ class new_context(object):
   def load(self,filename):
     with open(filename,'r') as f:
       for line in f:
-        if line.startswith('op'):              # hardwire in either: op |node> => ..., or |node> => ...
+        if line.startswith('op'):              # hardwire in either: "op |node> => ...", or "|node> => ..."
           line = line[2:]                      # maybe swap in ability to handle other length operators later, if useful.
         line = line.strip()
         if line.startswith('|'):
@@ -129,22 +143,46 @@ class new_context(object):
     for node,tail in self.rules.items():
       print('op |%s> => %s' % (node,str(tail)))
 
-  # node is a string
+  # node is a string, value is an integer
   def recall_node(self,node,value=1):
     if node in self.rules:
-      return self.rules[node].multiply(value)
+      if value == 1:
+        return self.rules[node]                      # no point doing multiplication if we don't need it.
+      else:
+        return self.rules[node].multiply(value)
     return superposition()
 
   # returns a list of known nodes:
   def nodes(self):
     return [node for node in self.rules]
 
+# define our node to signature function:
+# context is a context, node is a string, k is a positive integer, m is the modulus
+#
+def node_to_signature_v1(context,node,k,m):
+  signature = 1
+  r = superposition([node])
+  for n in range(0,2*k+2,2):
+    v1 = r.count()
+    v2 = r.count_sum()
+    signature = ((signature % m) * pow(primes[n],v1,m) ) % m
+    signature = ((signature % m) * pow(primes[n+1],v2,m) ) % m
+#    print("n:",n)
+#    print("r:",r)
+#    print("v1:",v1)
+#    print("v2:",v2)
+#    print("signature:",signature)
+    r1 = superposition()
+    for node,value in r.odict.items():
+      r1 += context.recall_node(node,value)
+    r = r1
+  return signature
 
 
 # define our node to signature function:
-# context is a context, node is a string, k is a positive integer
+# context is a context, node is a string, k is a positive integer, m is the modulus
 #
-def node_to_signature(context,node,k):
+def node_to_signature_v2(context,node,k,m):
   signature = 1
   r = superposition([node])
   v_list = []
@@ -154,29 +192,28 @@ def node_to_signature(context,node,k):
     v_list.append(v1)
     v_list.append(v2)
 
-    print("n:",n)
-    print("r:",r)
-    print("v1:",v1)
-    print("v2:",v2)
+#    print("n:",n)
+#    print("r:",r)
+#    print("v1:",v1)
+#    print("v2:",v2)
     r1 = superposition()
     for node,value in r.odict.items():
       r1 += context.recall_node(node,value)
     r = r1
-#    r = r.apply_op(context,op)
 
-  print("v_list:",v_list)
+#  print("v_list:",v_list)
 #  v_list.sort(reverse=True)        # we reverse sort the list, so largest v's are applied to smallest primes. This is quite a big saving.
-  v_list.reverse()                  # just reverse the list. Yeah, slightly bigger integers than reverse-sort, but reduces the chance of an accidental collision.
-  print("v_list:",v_list)
+#  v_list.reverse()                  # just reverse the list. Yeah, slightly bigger integers than reverse-sort, but reduces the chance of an accidental collision.
+#  print("v_list:",v_list)
   for i,v in enumerate(v_list):
     signature = ((signature % m) * pow(primes[i],v,m) ) % m
-  print("signature:",signature)
+#  print("signature:",signature)
   return signature
 
 
 # filename is a string, k is a positive integer
 #
-def file_to_hash(filename,k):
+def file_to_hash(filename,k,m):
   context = new_context()
   context.load(filename)
 
@@ -185,8 +222,8 @@ def file_to_hash(filename,k):
   # walk the network:
   # NB: multiplication is Abelian, so the order we examine nodes does not matter.
   for node in context.nodes():
-    signature = ((signature % m) * node_to_signature(context,node,k) ) % m
-  print("sig:",signature)
+    signature = ((signature % m) * node_to_signature_v1(context,node,k,m) ) % m
+#  print("sig:",signature)
   return hashlib.sha1(str(signature).encode('utf-8')).hexdigest()
 
 
@@ -194,7 +231,7 @@ network_classes = OrderedDict()
 for name in list_of_files:
   base = os.path.basename(name)
   base, ext = base.rsplit('.',1)
-  file_hash = file_to_hash(name,k)
+  file_hash = file_to_hash(name,k,m)
   if file_hash in network_classes:
     network_classes[file_hash].append(base)
   else:
@@ -207,6 +244,7 @@ for hash in network_classes:
   print(hash + ": " + ", ".join(the_class))
 print("----------------------------\n")
 
+#sys.exit(0)
 print("2nd-order-k%s-network:" % str(k))
 for i,hash in enumerate(network_classes):
   the_class = network_classes[hash]
@@ -216,81 +254,4 @@ for i,hash in enumerate(network_classes):
 print()
 for i,hash in enumerate(network_classes):
   print('hash |%s> => |%s>' % (str(i+1),hash))
-
-
-sys.exit(0)
-context = new_context()
-context.load(list_of_files[0])
-context.print()
-
-r = superposition(['B','C','G'])
-print("r:",r)
-
-r1 = superposition()
-for node,value in r.odict.items():
-  r1 += context.recall_node(node,value)
-print("r1:",r1)
-
-r2 = superposition()
-for node,value in r1.odict.items():
-  r2 += context.recall_node(node,value)
-print("r2:",r2)
-
-r = r2
-print("r:",r)
-
-sys.exit(0)
-print("nodes:",context.nodes())
-print("node 3:",context.recall_node('3'))
-print("node 5:",context.recall_node('5',11))
-print("node 23:",context.recall_node('23'))
-#sys.exit(0)
-
-r0 = superposition().multiply(5)
-r1 = superposition(['fish','cat','fish','fish',3,5]).multiply(5)
-r2 = r1 + r0 + r1 + r1
-print("r0:",r0)
-print("count r0:",r0.count())
-print("count_sum r0:",r0.count_sum())
-print("r1:",r1)
-print("count r1:",r1.count())
-print("count_sum r1:",r1.count_sum())
-print("r2:",r2)
-print("count r2:",r2.count())
-print("count_sum r2:",r2.count_sum())
-
-sys.exit(0)
-
-class int_ket(object):
-  def __init__(self,label='?',value=1):
-    self.label = label
-    self.value = int(value)
-
-  def __len__(self):
-    if self.label == '':
-      return 0
-    return 1
-
-  def __iter__(self):
-    yield ket(self.label,self.value)
-
-  def __str__(self):
-    return self.display()
-
-  def display(self):
-    if self.value == 1:
-      return '|%s>' % self.label
-    else:
-      return '%s|%s>' % (str(self.value),self.label)
-
-  def __add__(self,x):
-    return superposition() + self + x
-
-  def count(self):
-    if self.label == "":
-      return 0
-    return 1
-
-  def count_sum(self):
-    return self.value
 
