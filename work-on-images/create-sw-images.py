8fa7ef4ea6ecdd33615d39d309889cafdf705ac5
Garry Morrison <garry@semantic-db.org>
2016-05-04 20:33:14 +0930

    RGB version of create-sw-images.py

--- work-on-images/create-sw-images.py
+++ work-on-images/create-sw-images.py
@@ -31,7 +31,11 @@ context = context_list("average categorized rescaled superpositions to images")
 #context.load("sw-examples/mnist-100--save-average-categorize--0_5.sw")
 #context.load("sw-examples/mnist-100--save-average-categorize--0_6.sw")
 #context.load("sw-examples/mnist-100--save-average-categorize--0_7.sw")
-context.load("sw-examples/mnist-100--save-average-categorize--0_65.sw")
+#context.load("sw-examples/mnist-100--save-average-categorize--0_65.sw")
+#context.load("sw-examples/mnist-1000--layer-1--0_5.sw")
+#context.load("sw-examples/small-lenna-edge-40--layer-1--0_7.sw")
+context.load("sw-examples/small-lenna-edge-40--layer-1--0_4.sw")
+
 
 #filename = sys.argv[1]
 #op = "rescaled-layer-1"
@@ -48,13 +52,28 @@ def sp_to_image(sp,count):                  # assumes the sp is rescaled to 255 
   data = [ int(x.value) for x in sp ]
   im = Image.new('L',size)                  # assume this, rather than RGB. Will work for now.
   im.putdata(data)
-  im.save(destination + "mnist-100--average-image-%s.bmp" % count)
+  im.save(destination + "mnist-1000--average-image-%s.bmp" % count)
+
+
+def sp_to_rgb_image(sp,count):                  # assumes the sp is rescaled to 255 (so range is [0,255] )
+  d = 10                                    # hard wire in d = 10. This fixed the bug.
+  if len(sp) != 300:         
+    print("len sp:",len(sp))
+  size = (d,d)
+  data = [ int(x.value) for x in sp ]
+  im_data = [ (data[i],data[i+1],data[i+2]) for i in range(0,len(data),3) ]
+  im = Image.new('RGB',size)
+#  im.putdata(data)
+  im.putdata(im_data)
+  im.save(destination + "small-lenna-edge-40--average-image-%s.bmp" % count)
+
 
 count = 0
 for elt in context.relevant_kets(op):
   count += 1
   print("elt:",elt)
   our_sp = context.recall(op,elt).rescale(255)
-  sp_to_image(our_sp,count)
+#  sp_to_image(our_sp,count)
+  sp_to_rgb_image(our_sp,count)
   print("sp:",our_sp)
 
